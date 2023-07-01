16a599556f764fae48b3f90daecc021a951effe2
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-07-01 12:37:29 +0300

    crating react App

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ res.send(notes)
 app.get('/api/notes/:id',(req,res)=>{
 const note = notes.find((n)=>n._id=== req.params.id);
 res.send(note);
-});
+}); 
+
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,console.log(`server starrted at port ${PORT}`));
